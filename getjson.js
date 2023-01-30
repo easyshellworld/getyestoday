ef7347736b8018e072246547e1fd39260476e791
Your Name <you@example.com>
2023-01-31 07:43:35 +0800

    change getjson2

--- getjson.js
+++ getjson.js
@@ -7,7 +7,7 @@ function gettoday(coinname){
     //console.log(today)
    // console.log(last)
     let test= "https://api.mytokenapi.com/currency/kline?com_id="+coinname+"_usdt&symbol="+coinname+"&anchor=USDT&time="+last+"&market_id=338&period=1d&timestamp=1674739035146&code=ebc161c4c01e448626c3cc30518009d6&platform=web_pc&v=1.0.0&language=en_US&legal_currency=USD"
-   // let test= "https://www.runoob.com/try/ajax/json_demo.json"
+  // let test= "http://www.runoob.com/try/ajax/json_demo.json"
    // console.log(test)
     return test
   }
@@ -23,11 +23,11 @@ $(document).ready(function(){
 $.ajax({
    type:"get",
    url:gettoday("btc"),/*url写异域的请求地址*/
-   dataType:"jsonp",/*加上datatype*/
-   jsonpCallback:"cb",/*设置一个回调函数，名字随便取，和下面的函数里的名字相同就行*/
-   success:function(cb){
-      $("#coin").html("btc:"+cb.data.kline[1].close);
-      console.log(cb)
+   dataType:"json",/*加上datatype*/
+   jsonpCallback:"getdata",/*设置一个回调函数，名字随便取，和下面的函数里的名字相同就行*/
+   success:function(res){
+     // $("#coin").html("btc:"+cb.data.kline[1].close);
+      //console.log(res)
    }
   });
 
